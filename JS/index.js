44d31955108df2b333c1563d98c43b62d9fbf5e7
Baptiste Wagee <wageebaptiste.pro@gmail.com>
2026-06-27 16:51:54 +0200

    Correction d'un bug

--- JS/index.js
+++ JS/index.js
@@ -2,7 +2,7 @@ const logo = document.getElementById("logo-container");
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 50) {  // Déclenche plus tôt (50px au lieu de 150px)
         navbar.classList.add("scrolled");
         logo.classList.add("sticky");
     } else {
