ef0a3da8b1da5ad86ddf626aeee4c8255a6cd799
Baptiste Wagee <wageebaptiste.pro@gmail.com>
2026-06-27 16:31:22 +0200

    amelioration de la page principal

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,12 @@
+const logo = document.getElementById("logo-container");
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", () => {
-
-    if (window.scrollY > 80) {
-
+    if (window.scrollY > 150) {
         navbar.classList.add("scrolled");
-
+        logo.classList.add("sticky");
     } else {
-
         navbar.classList.remove("scrolled");
-
+        logo.classList.remove("sticky");
     }
-
 });
